1b6e1c0968f6b9633283f7b3f2eca4556dba635d
evgeniy <not-found@inbox.ru>
2024-01-13 22:52:09 +0300

    Added code according to video 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import React, {useState} from 'react';
-import styles from "./components/Site.module.css";
-
+import React from 'react'
+import styles from './components/Site.module.css'
+import { PageOne } from './components/pages/PageOne'
+import { PageTwo } from './components/pages/PageTwo'
+import { PageThree } from './components/pages/PageThree'
+import { Route, Routes } from 'react-router-dom'
+import {Error404} from './components/pages/Error404';
 
 function App() {
-    return (
-        <div>
-            <div className={styles.header}><h1>HEADER</h1></div>
-            <div className={styles.body}>
-                <div className={styles.nav}>
-                    Здесь будет навигация
-                </div>
-                <div className={styles.content}>
-                    Здесь будут кроссовки
-                </div>
-            </div>
-            <div className={styles.footer}>abibas 2023</div>
-        </div>
-    );
+	return (
+		<div>
+			<div className={styles.header}>
+				<h1>HEADER</h1>
+			</div>
+			<div className={styles.body}>
+				<div className={styles.nav}>Здесь будет навигация</div>
+				<div className={styles.content}>
+					<Routes>
+						<Route path={'/adidas'} element={<PageOne />}></Route>
+						<Route path={'/puma'} element={<PageTwo />}></Route>
+						<Route path={'/abibas'} element={<PageThree />}></Route>
+						<Route path={'/*'} element={<Error404 />}></Route>
+					</Routes>
+				</div>
+			</div>
+			<div className={styles.footer}>abibas 2023</div>
+		</div>
+	)
 }
 
-
-export default App;
+export default App
