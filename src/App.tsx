38ad1ee155c40f8aa1071307e28dbb532c74957f
evgeniy <not-found@inbox.ru>
2024-01-14 00:25:33 +0300

    Added code according to video 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import styles from './components/Site.module.css'
 import { PageOne } from './components/pages/PageOne'
 import { PageTwo } from './components/pages/PageTwo'
 import { PageThree } from './components/pages/PageThree'
-import { Route, Routes } from 'react-router-dom'
+import {Navigate, Route, Routes} from 'react-router-dom'
 import {Error404} from './components/pages/Error404';
 
 function App() {
@@ -16,10 +16,14 @@ function App() {
 				<div className={styles.nav}>Здесь будет навигация</div>
 				<div className={styles.content}>
 					<Routes>
+						<Route path={'/'} element={<Navigate to={'/adidas'}/>}></Route>
+
 						<Route path={'/adidas'} element={<PageOne />}></Route>
 						<Route path={'/puma'} element={<PageTwo />}></Route>
 						<Route path={'/abibas'} element={<PageThree />}></Route>
-						<Route path={'/*'} element={<Error404 />}></Route>
+
+						<Route path={'/error404'} element={<Error404 />}></Route>
+						<Route path={'/*'} element={<Navigate to={'/error404'}/>}></Route>
 					</Routes>
 				</div>
 			</div>
