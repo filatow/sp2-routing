9b474a8a32fb5952e98b38517346536bd9ef9ea2
filatow <not-found@inbox.ru>
2024-12-13 01:50:29 +0300

    Added code according to video 10

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
 import React from 'react'
-import {Adidas} from './components/pages/Adidas'
-import {Puma} from './components/pages/Puma'
-import {Navigate, NavLink, Route, Routes} from 'react-router-dom'
-import {Error404} from './components/pages/Error404'
+import {NavLink, Outlet} from 'react-router-dom'
 import {S} from './components/pages/_styles'
-import {Model} from './components/pages/Model';
-import {Prices} from './components/pages/Prices';
-
-const PATH = {
-  PAGE1: '/adidas',
-  PAGE2: '/puma',
-  PAGE3: '/abibas',
-  PRICES: '/prices',
-  ER404: '/page/error',
-} as const
+import {PATH} from './routes/router';
 
 export default function App() {
   return (
@@ -24,34 +12,20 @@ export default function App() {
       <S.Body>
         <S.Nav>
           <S.NavWrapper>
-            <NavLink to={PATH.PAGE1}> Adidas</NavLink>
+            <NavLink to={PATH.ADIDAS}> Adidas</NavLink>
           </S.NavWrapper>
           <S.NavWrapper>
-            <NavLink to={PATH.PAGE2}> Puma</NavLink>
+            <NavLink to={PATH.PUMA}> Puma</NavLink>
           </S.NavWrapper>
           <S.NavWrapper>
-            <NavLink to={PATH.PAGE3}> Abibas</NavLink>
+            <NavLink to={PATH.ABIBAS}> Abibas</NavLink>
           </S.NavWrapper>
           <S.NavWrapper>
             <NavLink to={PATH.PRICES}> Prices</NavLink>
           </S.NavWrapper>
         </S.Nav>
         <S.Content>
-          <Routes>
-            <Route path={'/'} element={<Navigate to={PATH.PAGE1}/>}></Route>
-
-            <Route path={PATH.PAGE1} element={<Adidas/>}></Route>
-            {/*<Route path={`${PATH.PAGE1}/:id`} element={<Model />}></Route>*/}
-            <Route path={PATH.PAGE2} element={<Puma/>}></Route>
-            {/*<Route path={`${PATH.PAGE2}/:id`} element={<Model />}></Route>*/}
-            <Route path={`/:producer/:id`} element={<Model/>}></Route>
-
-            <Route path={PATH.PRICES} element={<Prices/>}></Route>
-
-            <Route path={'/*'} element={<Error404/>}></Route>
-            {/*<Route path={PATH.ER404} element={<Error404 />}></Route>*/}
-            {/*<Route path={'/*'} element={<Navigate to={PATH.ER404} />}></Route>*/}
-          </Routes>
+          <Outlet/>
         </S.Content>
       </S.Body>
       <S.Footer>abibas 2024</S.Footer>
---
+++ src/routes/router.tsx
@@ -0,0 +1,47 @@
+import {createBrowserRouter,} from 'react-router-dom';
+import {Error404} from '../components/pages/Error404';
+import React from 'react';
+import {Model} from '../components/pages/Model';
+import {Adidas} from '../components/pages/Adidas';
+import {Puma} from '../components/pages/Puma';
+import {Abibas} from '../components/pages/Abibas';
+import {Prices} from '../components/pages/Prices';
+import App from '../App';
+
+export const PATH = {
+  ADIDAS: '/adidas',
+  PUMA: '/puma',
+  ABIBAS: '/abibas',
+  PRICES: '/prices',
+  ER404: '/page/error',
+} as const
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Error404 />,
+    children: [
+      {
+        path: PATH.ADIDAS,
+        element: <Adidas/>,
+      },
+      {
+        path: PATH.PUMA,
+        element: <Puma/>,
+      },
+      {
+        path: PATH.ABIBAS,
+        element: <Abibas/>,
+      },
+      {
+        path: PATH.PRICES,
+        element: <Prices/>,
+      },
+      {
+        path: "/:producer/:id",
+        element: <Model/>,
+      },
+    ],
+  },
+]);
